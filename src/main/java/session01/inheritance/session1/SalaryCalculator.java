f086c57658e3624a575409161568a6249d6b6671
Samet Yaprak <syaprak@indracompany.com>
2024-12-08 20:58:00 +0100

    this is my second commit

--- src/main/java/session01/inheritance/session1/SalaryCalculator.java
+++ src/main/java/session01/inheritance/session1/SalaryCalculator.java
@@ -2,10 +2,20 @@ package session01.inheritance.session1;
 
 public class SalaryCalculator {
 
-    //int monthlyProfit = 5;
+    int monthlyProfit = 5;
 
     void companySalaryCalculator(Worker worker){
         System.out.println(worker.calculateSalary());
+
+
+
+
+
+
+
+
+
+
     }
 
 
