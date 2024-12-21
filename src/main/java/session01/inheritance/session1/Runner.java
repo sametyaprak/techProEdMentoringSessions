db2ed42c7329197e89f9fb16696e686a812b1f67
Samet Yaprak <syaprak@indracompany.com>
2024-12-21 21:30:01 +0100

    Refactor and clean up Runner.java for improved clarity
    Enhanced code readability by organizing comments and fixing formatting issues. Duplicated list declarations were removed, and unnecessary blank lines were adjusted. Minor improvements support better maintainability and code style consistency.

--- src/main/java/session01/inheritance/session1/Runner.java
+++ src/main/java/session01/inheritance/session1/Runner.java
@@ -6,18 +6,32 @@ public class Runner {
 
 
     public static void main(String[] args) {
-
-
-
         //this is an abstract class
         //we can not create an object
         //BasePerson person =  new BasePerson();
-
-
         SalaryCalculator calculator = new SalaryCalculator();
         Worker secretary = new Manager();
         //! companySalaryCalculator method call calculateSalary() method but
         //! java does not know which method will be called.(we have overwritten methods)
+			public static Optional<String> getCurrentUserLogin(){
+				SecurityContext securityContext= SecurityContextHolder.getContext();
+				Authentication authentication = securityContext.getAuthentication();
+				return Optional.ofNullable(extractPrincipal(authentication));
+			}
+
+			private static String extractPrincipal(Authentication authentication) {
+				if(authentication==null) {
+					return null;
+				}else if (authentication.getPrincipal() instanceof UserDetails) {
+					UserDetails securedUser= (UserDetails) authentication.getPrincipal();
+					return securedUser.getUsername();
+				}else if(authentication.getPrincipal() instanceof String) {
+					return  (String)authentication.getPrincipal();
+				}
+
+				return null;
+
+			}
         calculator.companySalaryCalculator(secretary);
         if(secretary instanceof Manager ){
             System.out.println("manager");
@@ -26,37 +40,10 @@ public class Runner {
         } else  if(secretary instanceof Worker ){
             System.out.println("worker");
         }
-        }	public ByteArrayInputStream getUserReport() {
-		List<User> users = userService.getUsers();
-		try {
-			return ExcelReporter.getUserExcelReport(users);
-		} catch (IOException e) {
-			throw new RuntimeException(ErrorMessage.EXCEL_REPORT_ERROR_MESSAGE);
-		}
-	}
-	public ByteArrayInputStream getCarReport() {
-		List<Car> cars = carService.getAllCar();
-		try {
-			return ExcelReporter.getCarExcelReport(cars);
-		} catch (IOException e) {
-			throw new RuntimeException(ErrorMessage.EXCEL_REPORT_ERROR_MESSAGE);
-		}
-	}
-	public ByteArrayInputStream getReservationReport() {
-		List<Reservation> reservations = reservationService.getAll();
-		try {
-			return ExcelReporter.getReservationExcelReport(reservations);
-		} catch (IOException e) {
-			throw new RuntimeException(ErrorMessage.EXCEL_REPORT_ERROR_MESSAGE);
-		}
-	}
         //polymorphism examples in core java library.
         List<Secretary>secretaries1 = new ArrayList<>();
         List<Secretary>secretaries2 = new LinkedList<>();
         List<Secretary>secretaries3 = new Vector<>();
-        //polymorphism examples in core java library.
-        List<Secretary>secretaries1 = new ArrayList<>();
-        List<Secretary>secretaries2 = new LinkedList<>();
-        List<Secretary>secretaries3 = new Vector<>();
+
     }
 }
